989f439544a48bde80b545f43c170eb595b26c8c
Sidra-009 <sidrasaqlain11@gmail.com>
2025-12-17 23:29:30 +0500

    Bump version to 0.3.4 for PyPI upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mathnexus", 
-    version="0.3.3",   
+    version="0.3.4",   
     author="Sidra Saqlain",
     author_email="sidrasaqlain11@gmail.com", 
     description="A library for Linear Algebra, 2D Geometry, and Physics Simulations.",
